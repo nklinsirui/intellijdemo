13f42ac0a7db49d8154709243e79d18e9b298674
crazymurex <crazymurex@sina.com>
2017-05-21 14:47:45 +0800

    Add light class

---
+++ src/com/linsiui/Light.java
@@ -0,0 +1,10 @@
+package com.linsiui;
+
+/**
+ * Created by linsirui on 2017/5/21.
+ */
+public class Light {
+    public static void main(String[] args) {
+        System.out.println("Opple light");
+    }
+}
